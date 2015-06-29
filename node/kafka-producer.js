1cb34bf2c45935feaf2af01b885c180acc0d9a00
Marc Bogaerts <marcbogaerts@telenet.be>
2015-06-29 23:04:35 +0200

    cluster kafka producer

--- node/kafka-producer.js
+++ node/kafka-producer.js
@@ -1,20 +1,32 @@
 var kafka = require('kafka-node'),
+    cluster = require('cluster'),
     Producer = kafka.Producer,
     client = new kafka.Client(),
-    producer = new Producer(client),
+    
     payloads = [
         { topic: 'test', messages: 'hello world', partition: 0, attributes: 0 }
     ];
 
-var MAX = 10000;
-var sent = 0;
+var WORKERS = 6;
 
-function send(payloads, cnt) {
+if (cluster.isMaster) {
+    for (var i = 0; i < WORKERS; i++) {
+        var worker = cluster.fork();
+    }
+} else {
+    console.log('I am worker #' + cluster.worker.id);
+ 
+var MAX = 5000;
+var sent = 0;
+var producer = new Producer(client);
+
+function send (payloads, cnt) {
     producer.send(payloads, function (err, data) {
         // console.log(data);
         if (cnt == MAX) {
 		client.close(function() {
                     console.log('Done');
+                    cluster.worker.disconnect();
                 });
         }
     });
@@ -30,4 +42,15 @@ producer.on('ready', function () {
  
 producer.on('error', function (err) {
     console.log(err);
+});
+
+}
+
+var done = 0;
+cluster.on('disconnect', function(worker) {
+    console.log('The worker #' + worker.id + ' has disconnected');
+    done++;
+    if (done == WORKERS) {
+        process.exit(0);
+    }
 });
